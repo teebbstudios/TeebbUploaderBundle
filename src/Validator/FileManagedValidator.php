6d689827aaee4cb7f6195588e030fdf52c3b43fc
伟伟权 <qww.zone@gmail.com>
2022-02-23 17:26:12 +0800

    12.在Bundle中添加自定义验证器

---
+++ src/Validator/FileManagedValidator.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Teebb\UploaderBundle\Validator;
+
+use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\ConstraintValidator;
+use Teebb\UploaderBundle\Entity\File;
+
+class FileManagedValidator extends ConstraintValidator
+{
+    public function validate($value, Constraint $constraint)
+    {
+//        dd($value, $constraint);
+        /* @var $constraint \App\Validator\FileManaged */
+
+        if (null === $value || '' === $value) {
+            return;
+        }
+//dd($value);
+//        foreach ($value as $file)
+//        {
+            if ($value instanceof File){
+                $mimeType = $value->getMimeType();
+
+                foreach ($constraint->mimeTypes as $allowMimeType){
+                    if ($allowMimeType === $mimeType){
+                        return;
+                    }
+
+                    if ($discrete = strstr($allowMimeType, '/*', true)) {
+                        if (strstr($mimeType, '/', true) === $discrete) {
+                            return;
+                        }
+                    }
+
+                }
+//                if ($prestr = strstr($mimeType, '/', true)){
+//                    if ($prestr === 'image'){
+//                        return;
+//                    }
+//                }
+            }
+//        }
+
+        // TODO: implement the validation here
+        $this->context->buildViolation($constraint->message)
+            ->setParameter('{{ name }}', $value->getOriginName())
+            ->addViolation();
+    }
+}
